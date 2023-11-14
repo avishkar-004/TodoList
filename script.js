bc5585438ab33d8ed39027f1f51e296b75120280
Avishkar Pawar <avishkarpawar004@gmail.com>
2023-11-14 16:05:31 +0530

    feat: add localStorage persistence for tasks

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
 var taskInput = document.getElementById('taskInput');
 var addBtn = document.getElementById('addBtn');
 var taskList = document.getElementById('taskList');
+var tasks = JSON.parse(localStorage.getItem('tasks')) || [];
 
 addBtn.addEventListener('click', addTask);
 taskInput.addEventListener('keypress', function(e) {
   if (e.key === 'Enter') addTask();
 });
+
+function saveTasks() {
+  var data = [];
+  var items = taskList.querySelectorAll('.task');
+  items.forEach(function(item) {
+    data.push({
+      text: item.querySelector('span').textContent,
+      completed: item.classList.contains('completed')
+    });
+  });
+  localStorage.setItem('tasks', JSON.stringify(data));
+}
 
 function addTask() {
   var text = taskInput.value.trim();
@@ -13,14 +26,21 @@ function addTask() {
     alert('Please enter a task.');
     return;
   }
+  createTaskElement(text, false);
+  taskInput.value = '';
+  saveTasks();
+}
 
+function createTaskElement(text, completed) {
   var li = document.createElement('li');
   li.className = 'task';
+  if (completed) li.classList.add('completed');
 
   var span = document.createElement('span');
   span.textContent = text;
   span.addEventListener('click', function() {
     li.classList.toggle('completed');
+    saveTasks();
   });
 
   var deleteBtn = document.createElement('button');
@@ -28,10 +48,18 @@ function addTask() {
   deleteBtn.className = 'delete-btn';
   deleteBtn.addEventListener('click', function() {
     li.remove();
+    saveTasks();
   });
 
   li.appendChild(span);
   li.appendChild(deleteBtn);
   taskList.appendChild(li);
-  taskInput.value = '';
 }
+
+function loadTasks() {
+  tasks.forEach(function(task) {
+    createTaskElement(task.text, task.completed);
+  });
+}
+
+loadTasks();
